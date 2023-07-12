da2dff8f29ada373a9e6f9611652affecddfe9da
sampletestgh <lakshmi.vinu19@gmail.com>
2023-07-12 09:50:03 +0530

    fstcommit

---
+++ src/basic/MockPrgm.java
@@ -0,0 +1,15 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class MockPrgm {
+
+	public static void main(String[] args) {
+		WebDriver driver=new EdgeDriver();
+		//driver.get("https://demo.actitime.com/login.do");
+		//driver.manage().window().maximize();
+	}
+
+}
